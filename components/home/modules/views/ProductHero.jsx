e93e4c14ffdca2c342696767811bb4c0a3f3696d
Alan Nguyen <a_wen197@yahoo.com>
2024-02-14 14:11:58 -0800

    changed image

--- components/home/modules/views/ProductHero.jsx
+++ components/home/modules/views/ProductHero.jsx
@@ -4,7 +4,7 @@ import Typography from '../components/Typography';
 import ProductHeroLayout from './ProductHeroLayout';
 
 const backgroundImage =
-  'https://images.unsplash.com/photo-1534854638093-bada1813ca19?auto=format&fit=crop&w=1400';
+  'https://i.ibb.co/PTdCLKt/scenery.jpg';
 
 export default function ProductHero() {
   return (
